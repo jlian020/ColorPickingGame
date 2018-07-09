7e76860ff1848391d3c1f4778e598f989790be1e
David Liang <jlian020@ucr.edu>
2018-07-09 14:15:36 -0700

    FINISHED PROJECT

--- script.js
+++ script.js
@@ -3,19 +3,45 @@ var rgbDisplay = document.querySelector("#rgbDisplay");
 var title = document.querySelector("#title");
 var resetBoardButton = document.querySelector("#newBoard");
 var barMessage = document.querySelector("#message");
+var easybutton = document.querySelector("#easybutton");
+var hardbutton = document.querySelector("#hardbutton");
 
 var gameOver = false;
+var numTiles = 6;
 var chosenColor = "";
 
-newColors();
-chooseColor();
+newColors(6);
+chooseColor(6);
 
 resetBoardButton.addEventListener("click", function() {
-	newColors();
-	chooseColor();
-	title.style.backgroundColor = "black";
-	gameOver = false;
-	barMessage.textContent = "";
+	reset();
+});
+
+easybutton.addEventListener("click", function() {
+	easybutton.classList.add("active");
+	hardbutton.classList.remove("active");
+
+	numTiles = 3;
+
+	reset();
+
+	squares[3].style.display = "none";
+	squares[4].style.display = "none";
+	squares[5].style.display = "none";
+
+});
+
+hardbutton.addEventListener("click", function() {
+	easybutton.classList.remove("active");
+	hardbutton.classList.add("active");
+
+	numTiles = 6;
+
+	reset();
+
+	squares[3].style.display = "initial";
+	squares[4].style.display = "initial";
+	squares[5].style.display = "initial";
 });
 
 // Make squares dissapear if it is not the correct one
@@ -46,13 +72,23 @@ function changeAll(color) {
 	}
 }
 
-function chooseColor() {
-	chosenColor = squares[Math.floor(Math.random() * squares.length)].style.backgroundColor;
+function chooseColor(num) {
+	chosenColor = squares[Math.floor(Math.random() * num)].style.backgroundColor;
 	rgbDisplay.textContent = chosenColor;
 }
 
-function newColors() {
-	for(var i = 0; i < squares.length; i++) {
+function newColors(num) {
+	for(var i = 0; i < num; i++) {
 		squares[i].style.backgroundColor = "rgb(" + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ")";
 	}
 }
+
+function reset() {
+	barMessage.textContent = "";
+	title.style.backgroundColor = "black";
+	gameOver = false;
+	newColors(numTiles);
+	chooseColor(numTiles);
+}
+
+console.log(squares.length);
